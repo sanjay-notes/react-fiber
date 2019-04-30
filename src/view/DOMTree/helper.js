60ffa24eb8694c2b70803a15c7a4cdb4cfe18eaa
Anbalagan <sanbalagan@mabossanbala-m1.local>
2019-04-30 08:50:40 -0400

    Build generated for publish

--- src/view/DOMTree/helper.js
+++
@@ -1,8 +0,0 @@
-export function getBoundingRect(htmlElement){
-  return {
-    x: htmlElement ? htmlElement.offsetLeft : NaN,
-    y: htmlElement ? htmlElement.offsetTop :  NaN,
-    width:htmlElement ? htmlElement.offsetWidth :  NaN,
-    height:htmlElement ? htmlElement.offsetHeight :  NaN
-  };
-}
